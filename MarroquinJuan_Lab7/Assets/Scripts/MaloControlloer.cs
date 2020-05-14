440716f0ae09a4d656310daf595eb2f55617459e
Mar19845 <54874288+Mar19845@users.noreply.github.com>
2020-05-14 17:54:41 -0600

    creacion movimineto del malo

---
+++ MarroquinJuan_Lab7/Assets/Scripts/MaloControlloer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MaloControlloer : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    private List<GameObject> listwp;
+    public GameObject waypoints;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        listwp = new List<GameObject>();
+        
+        for(int i = 0;i<waypoints.transform.childCount; i++)
+        {
+            listwp.Add(waypoints.transform.GetChild(i).gameObject);
+        }
+
+        agent.SetDestination(listwp[Random.Range(0,listwp.Count-1)].transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(agent.remainingDistance<2f)
+        {
+            agent.SetDestination(listwp[Random.Range(0,listwp.Count-1)].transform.position);
+        }
+    }
+}
